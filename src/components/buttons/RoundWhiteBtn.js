1d9a4a43f3c01607c8b4c440a3bbac577c43052c
오채민 <ocmin20022@gmail.com>
2024-06-02 04:21:31 +0900

    Add: 동글 버튼 컴포넌트로 추가 (흰색/초록색)

---
+++ src/components/buttons/RoundWhiteBtn.js
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+
+
+export default function RoundGreenBtn({
+    text,
+    style,
+    onClick,
+}) {
+    return (
+        <Button style={style} onClick={onClick}>
+            {text}
+        </Button>
+    );
+}
+
+const Button = styled.div`
+    position: absolute;
+
+    // 버튼 꾸미기
+    background: #ffffff;
+    border-radius: 1.5625rem;
+
+    font-size: 1.25rem;
+    color: #6E6E6E;
+    line-height: 1.5rem;
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+
+    border: 0.1rem solid #AFBFA5;
+    cursor: pointer;
+`;
