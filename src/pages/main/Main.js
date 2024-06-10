48e319eac814ac2e2fb43124c2c9262973b563ef
오채민 <ocmin20022@gmail.com>
2024-06-11 01:48:26 +0900

    Fix: 불필요한 리액트 훅 제거

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import { styled } from "styled-components";
 import PageTitle from "../../components/PageTitle";
 import RoundGreenBtn from "../../components/buttons/RoundGreenBtn";
