053cbed56c11fe56414d2e81fda23b769c24c2a4
yeeunli <lye920@g.hongik.ac.kr>
2024-06-16 22:55:43 +0900

    Fix: placeholder2 컴포넌트 분리

--- src/components/PlaceHolder2.js
+++ src/components/PlaceHolder2.js
@@ -10,15 +10,16 @@ const Input = styled.input`
   box-sizing: border-box;
   width: 25.3125rem;
   height: 2.8125rem;
-  border: 1.5px solid #d9d9d9;
+  border: 0.1rem solid #afbfa5;
   border-radius: 15px;
   font-family: "Pretendard-Medium";
   font-size: 1.3125rem;
   line-height: 24px;
-  color: #6e6e6e;
+  color: #8aa353;
+  padding: 0 1rem;
 
   &::placeholder {
-    color: #6e6e6e;
-    font-size: 16px;
+    color: #afbfa5;
+    font-size: 1rem;
   }
 `;
