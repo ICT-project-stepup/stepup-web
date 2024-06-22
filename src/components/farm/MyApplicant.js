c967d841025351746d9eb7488b8082ae278ac896
yeeunli <lye920@g.hongik.ac.kr>
2024-06-23 03:26:57 +0900

    Feat: 농가 - 지원자 현황 화면 구현

---
+++ src/components/farm/MyApplicant.js
@@ -0,0 +1,64 @@
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import RoundWhiteBtn from "../../components/buttons/RoundWhiteBtn";
+
+export default function MyPost({ applicantData }) {
+  const navigate = useNavigate();
+
+  const handleResumeClick = () => {
+    navigate("/showresume");
+  };
+
+  return (
+    <PostListWrapper>
+      <span className="area">{applicantData.applicantArea}</span>
+      <span className="name" style={{ fontFamily: "Pretendard-SemiBold" }}>
+        {applicantData.applicantName}
+      </span>
+      <span className="age">{applicantData.applicantAge}</span>
+      <span className="gender">{applicantData.applicantGender}</span>
+      <span className="applyDate">{applicantData.applicantDate}</span>
+      <span className="resume">
+        <RoundWhiteBtn
+          text="이력서 보기"
+          onClick={handleResumeClick}
+          style={{
+            boxSizing: "border-box",
+            width: "100%",
+            maxWidth: "9.5rem", // 최대 크기 설정
+            height: "2.6875rem",
+            borderRadius: "0.9375rem",
+            color: "#6E6E6E",
+            fontFamily: "Pretendard-Medium",
+            lineHeight: "1.4925rem",
+            position: "relative",
+          }}
+        />
+      </span>
+    </PostListWrapper>
+  );
+}
+
+const PostListWrapper = styled.div`
+  width: 100%;
+  height: 4.2rem;
+  border-bottom: solid 0.0625rem #6e6e6e;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-decoration: none;
+  font-family: "Pretendard-Regular";
+  font-size: 1.375rem;
+  color: #2b2b2b;
+
+  .area,
+  .name,
+  .age,
+  .gender,
+  .applyDate,
+  .resume {
+    width: 12rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- src/pages/farm/ShowApplicant.js
+++ src/pages/farm/ShowApplicant.js
@@ -1,22 +1,122 @@
+import { React, useState } from "react";
 import { styled } from "styled-components";
+import PageTitle from "../../components/PageTitle";
+import CustomPagination from "../../components/CustomPagination";
+import MyApplicant from "../../components/farm/MyApplicant";
+
+const applicantData = [
+  {
+    applicantArea: "서울",
+    applicantName: "복귀주",
+    applicantAge: "31",
+    applicantGender: "남자",
+    applicantDate: "2024.05.16",
+  },
+  {
+    applicantArea: "경기 수원시",
+    applicantName: "송삼동",
+    applicantAge: "43",
+    applicantGender: "남자",
+    applicantDate: "2024.05.14",
+  },
+];
 
 /* 예은 */
 export default function ShowApplicant() {
-    return(
-        <Test>
-            <span>지원자 현황 - 농가</span>
-        </Test>
-    )
+  const [activePage, setActivePage] = useState(1);
+
+  /* 페이지네이션에 필요한 변수들 */
+  const totalItemsCount = applicantData.length;
+  const indexOfLastPost = activePage * 7;
+  const indexOfFirstPost = indexOfLastPost - 7;
+  const currentPosts = applicantData.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
+
+  const currentStatusPosts = currentPosts.filter((post) =>
+    applicantData.includes(post)
+  );
+
+  return (
+    <Container>
+      <PageTitle text="지원자 정보" style={{ position: "relative" }} />
+      <CountWrapper>
+        <span>총 {totalItemsCount}건</span>
+      </CountWrapper>
+      <ListWrapper>
+        <ListTitle>
+          <span className="area">지역</span>
+          <span className="name">이름</span>
+          <span className="age">나이</span>
+          <span className="gender">성별</span>
+          <span className="applyDate">지원일</span>
+          <span className="resume" />
+        </ListTitle>
+        {currentStatusPosts.map((applicantData, index) => (
+          <MyApplicant key={index} applicantData={applicantData} />
+        ))}
+        <CustomPagination
+          activePage={activePage}
+          totalItemsCount={totalItemsCount}
+          handlePageChange={handlePageChange}
+        />
+      </ListWrapper>
+    </Container>
+  );
 }
 
-const Test = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    margin-top: 10rem;
+const Container = styled.div`
+  width: auto;
+  height: auto;
+  display: block;
+  align-items: flex-start;
+  padding: 2rem 6rem 2rem 6rem;
+  font-family: Pretendard-Medium;
+  font-size: 1.5rem;
+  color: #2e2e2e;
+`;
 
-    font-family: "Pretendard-Bold";
-    font-size: 3rem;
-    color: #8AA353;
+const CountWrapper = styled.div`
+  width: 100%;
+  height: 1.625rem;
+  font-family: "Pretendard-Regular";
+  font-size: 1.375rem;
+  color: #6e6e6e;
+  display: flex;
+  align-items: center;
+  margin-top: 2.125rem;
+`;
+
+const ListWrapper = styled.div`
+  width: 100%;
+  display: block;
+  border-top: solid 0.125rem #2b2b2b;
+  margin-top: 1.6875rem;
+`;
+
+const ListTitle = styled.div`
+  border-bottom: solid 0.0625rem #2b2b2b;
+  margin-top: 1rem;
+  padding-bottom: 1rem; /* 아래쪽에 1rem 간격 추가 */
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: "Pretendard-Regular";
+  font-size: 1.5rem;
+
+  .area,
+  .name,
+  .age,
+  .gender,
+  .applyDate,
+  .resume {
+    width: 12rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
