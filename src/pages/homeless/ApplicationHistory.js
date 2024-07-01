93f61147718ef9423af1f0971eb7dda2d5ea86c4
오채민 <ocmin20022@gmail.com>
2024-07-02 05:42:41 +0900

    Feat: 구직자 지원 현황 구현

--- src/pages/homeless/ApplicationHistory.js
+++ src/pages/homeless/ApplicationHistory.js
@@ -1,123 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import PageTitle from "../../components/PageTitle";
 import ApplyStatus from "../../components/homeless/application/ApplyStatus";
-import ApplyHistory from "../../components/homeless/application/ApplyHistory";
 import CustomPagination from "../../components/CustomPagination";
 
-const applyStatusData = [
-  {
-    area: "경남 창녕군",
-    postTitle: "마늘 뽑으실 분 구합니다",
-    salaryType: "일급",
-    salary: "100,000",
-    workTime: "09:00 ~ 16:00",
-    postDate: "2024.05.17",
-    postState: "지원 중",
-  },
-  {
-    area: "전남 진도군",
-    postTitle: "대파 뽑으실 분 구해요",
-    salaryType: "시급",
-    salary: "9,600",
-    workTime: "08:00 ~ 14:00",
-    postDate: "2024.05.11",
-    postState: "성사됨",
-  },
-  {
-    area: "충북 제천시",
-    postTitle: "집 보수 도와주실 분",
-    salaryType: "일급",
-    salary: "70,000",
-    workTime: "08:00 ~ 18:00",
-    postDate: "2024.04.23",
-    postState: "마감",
-  },
-];
-
-const applyHistoryData = [
-  {
-    area: "경남 창녕군",
-    postTitle: "마늘 뽑으실 분 구합니다",
-    salaryType: "일급",
-    salary: "100,000",
-    workTime: "09:00 ~ 16:00",
-    postDate: "2024.03.17",
-    postState: "성사됨",
-  },
-  {
-    area: "전남 진도군",
-    postTitle: "대파 뽑으실 분 구해요",
-    salaryType: "시급",
-    salary: "9,600",
-    workTime: "08:00 ~ 14:00",
-    postDate: "2024.02.11",
-    postState: "성사됨",
-  },
-  {
-    area: "충북 제천시",
-    postTitle: "집 보수 도와주실 분",
-    salaryType: "일급",
-    salary: "70,000",
-    workTime: "08:00 ~ 18:00",
-    postDate: "2024.04.23",
-    postState: "마감",
-  },
-];
 
 /* 채은 */
 export default function ApplicationHistory() {
   const [activePage, setActivePage] = useState(1);
+  const [applyStatusPosts, setApplyStatusPosts] = useState([]);
   const itemsPerPage = 7;
 
-  const allData = [...applyStatusData, ...applyHistoryData];
-  const totalItemsCount = allData.length;
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const userId = localStorage.getItem("id"); // 로컬 스토리지에서 id 가져오기
+      const response = await fetch(`/api/applicant/jobads/${userId}`);
+      if (!response.ok) {
+        throw new Error("서버에서 데이터를 가져오는 중 오류가 발생했습니다.");
+      }
+      const data = await response.json();
+      setApplyStatusPosts(data);
+    } catch (error) {
+      console.error("데이터를 불러오는 중 오류가 발생했습니다:", error);
+      // 오류 처리 로직 추가
+    }
+  };
+
+  /* 페이지네이션 변수들 */
+  const totalItemsCount = applyStatusPosts.length;
   const indexOfLastPost = activePage * itemsPerPage;
   const indexOfFirstPost = indexOfLastPost - itemsPerPage;
-  const currentPosts = allData.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = applyStatusPosts.slice(indexOfFirstPost, indexOfLastPost);
 
   const handlePageChange = (pageNumber) => {
     setActivePage(pageNumber);
   };
 
-  const currentStatusPosts = currentPosts.filter((post) =>
-    applyStatusData.includes(post)
-  );
-  const currentHistoryPosts = currentPosts.filter((post) =>
-    applyHistoryData.includes(post)
-  );
-
   return (
     <MainContainer>
-        <PageTitle text="지원 현황" />
-        <StatusListWrapper>
-          <ListTitle>
-            <span className="area">지역</span>
-            <span className="title">제목</span>
-            <span className="salary">급여</span>
-            <span className="time">시간</span>
-            <span className="date">등록일</span>
-            <span className="state">현황</span>
-          </ListTitle>
-          {currentStatusPosts.map((postInfo, index) => (
-            <ApplyStatus key={index} postInfo={postInfo} />
-          ))}
-        </StatusListWrapper>
-
-        <PageTitle text="지원 내역" />
-        <HistoryListWrapper>
-          <ListTitle>
-            <span className="area">지역</span>
-            <span className="title">제목</span>
-            <span className="salary">급여</span>
-            <span className="time">시간</span>
-            <span className="date">등록일</span>
-            <span className="state">현황</span>
-          </ListTitle>
-          {currentHistoryPosts.map((postInfo, index) => (
-            <ApplyHistory key={index} postInfo={postInfo} />
-          ))}
-        </HistoryListWrapper>
+      <PageTitle text="지원 현황" />
+      <StatusListWrapper>
+        <ListTitle>
+          <span className="area">지역</span>
+          <span className="title">제목</span>
+          <span className="salary">급여</span>
+          <span className="time">시간</span>
+          <span className="date">등록일</span>
+          <span className="state">현황</span>
+        </ListTitle>
+        {currentPosts.map((postInfo, index) => (
+          <ApplyStatus key={index} postInfo={postInfo} />
+        ))}
+      </StatusListWrapper>
 
       <CustomPagination
         activePage={activePage}
@@ -132,7 +70,7 @@ const MainContainer = styled.div`
     width: auto;
     display: block;
     align-items: flex-start;
-    padding: 2rem 6rem 0 6rem;
+    padding: 2rem 6rem 6rem 6rem;
 `;
 
 const StatusListWrapper = styled.div`
@@ -142,14 +80,6 @@ const StatusListWrapper = styled.div`
   margin-top: 5rem;
   margin-bottom: 5rem;
 `;
-
-const HistoryListWrapper = styled.div`
-  width: 100%;
-  display: block;
-  border-top: solid 0.2rem black;
-  margin-top: 10rem;
-`;
-
 
 const ListTitle = styled.div`
   border-bottom: solid 0.1rem black;
